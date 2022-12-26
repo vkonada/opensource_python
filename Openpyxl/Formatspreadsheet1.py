612625656840368e1362fe01600e5b32d77a9603
vkonada <70343697+vkonada@users.noreply.github.com>
2022-12-26 16:59:41 -0500

    Update Formatspreadsheet1.py

--- Openpyxl/Formatspreadsheet1.py
+++ Openpyxl/Formatspreadsheet1.py
@@ -22,3 +22,38 @@ sheet.column_dimensions['B'].width = 20
 
 # save the file
 wb.save('sample.xlsx')
+
+#################
+
+
+import openpyxl
+
+# import Font function from openpyxl
+from openpyxl.styles import Font
+
+
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+sheet.cell(row = 1, column = 1).value = "GeeksforGeeks"
+
+# set the size of the cell to 24
+sheet.cell(row = 1, column = 1).font = Font(size = 24 )
+
+sheet.cell(row = 2, column = 2).value = "GeeksforGeeks"
+
+# set the font style to italic
+sheet.cell(row = 2, column = 2).font = Font(size = 24, italic = True)
+
+sheet.cell(row = 3, column = 3).value = "GeeksforGeeks"
+
+# set the font style to bold
+sheet.cell(row = 3, column = 3).font = Font(size = 24, bold = True)
+
+sheet.cell(row = 4, column = 4).value = "GeeksforGeeks"
+
+# set the font name to 'Times New Roman'
+sheet.cell(row = 4, column = 4).font = Font(size = 24, name = 'Times New Roman')
+
+wb.save('sample.xlsx')
+
