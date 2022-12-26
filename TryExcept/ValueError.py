49eb7b3b1cb6db841a63fa2adb41768aca639dcf
vkonada <70343697+vkonada@users.noreply.github.com>
2022-12-26 17:11:10 -0500

    Create ValueError.py

---
+++ TryExcept/ValueError.py
@@ -0,0 +1,7 @@
+programming_languages = ["JavaScript","Python","Java","Python","C++","Python"]
+
+try:
+    print(programming_languages.index("React"))
+except ValueError:
+    print("That item does not exist")
+    
